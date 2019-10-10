92ba2cd6c838f52056f78a5b4e4550ecc465bcb1
Bradlee Speice <bradlee@speice.io>
2019-10-09 21:31:28 -0400

    ...and fix the test case

--- aeron_driver-sys/src/lib.rs
+++ aeron_driver-sys/src/lib.rs
@@ -13,7 +13,7 @@ mod tests {
         let minor = unsafe { crate::aeron_version_minor() };
         let patch = unsafe { crate::aeron_version_patch() };
         assert_eq!(major, 1);
-        assert_eq!(minor, 21);
-        assert_eq!(patch, 2);
+        assert_eq!(minor, 22);
+        assert_eq!(patch, 0);
     }
 }
